fb7d70dd5777a74353bd65292e9db6c5837b487e
Emir <e2guevar@uwaterloo.ca>
2026-02-05 18:31:44 -0500

    new changes

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -4,11 +4,11 @@ module spi_peripheral #(parameter W = 8) (
     input wire SCLK,
     input wire nCS,
     input wire COPI,
-    output reg [W-1:0] en_reg_out_7_0,
-    output reg [W-1:0] en_reg_out_15_8,
-    output reg [W-1:0] en_reg_pwm_7_0,
-    output reg [W-1:0] en_reg_pwm_15_8,
-    output reg [W-1:0] pwm_duty_cycle
+    output [W-1:0] en_reg_out_7_0,
+    output [W-1:0] en_reg_out_15_8,
+    output [W-1:0] en_reg_pwm_7_0,
+    output [W-1:0] en_reg_pwm_15_8,
+    output [W-1:0] pwm_duty_cycle
 );
 
 //max address for registers
@@ -52,30 +52,17 @@ assign en_reg_pwm_15_8 = registers[3];
 assign pwm_duty_cycle = registers[4];
 
 //synchronizer
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        sclk_sync0 <= 0;
-        ncs_sync0 <= 1;
-        copi_sync0 <= 0;
+always @(posedge clk) begin
+    sclk_sync0 <= SCLK;
+    ncs_sync0 <= nCS;
+    copi_sync0 <= COPI;
+    
+    sclk_sync1 <= sclk_sync0;
+    ncs_sync1 <= ncs_sync0;
+    copi_sync1 <= copi_sync0;
 
-        sclk_sync1 <= 0;
-        ncs_sync1 <= 1;
-        copi_sync1 <= 0;
-
-        sclk_sync2 <= 0;
-        ncs_sync2 <= 1;
-    end else begin
-        sclk_sync0 <= SCLK;
-        ncs_sync0 <= nCS;
-        copi_sync0 <= COPI;
-        
-        sclk_sync1 <= sclk_sync0;
-        ncs_sync1 <= ncs_sync0;
-        copi_sync1 <= copi_sync0;
-
-        sclk_sync2 <= sclk_sync1;
-        ncs_sync2 <= ncs_sync1;
-    end
+    sclk_sync2 <= sclk_sync1;
+    ncs_sync2 <= ncs_sync1;
 end
 
 //edge detection logic
